641f2c3d61c9533fdf4986bf97af30cf13512f46
Christy Fury <christy.fury@gmail.com>
2023-07-15 16:57:37 -0400

    adds canton point and popup to map

--- bottoms_up/frontend/src/componenets/MapComp.js
+++ bottoms_up/frontend/src/componenets/MapComp.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './Map.css'
 
 const LeafletMap = () => {
@@ -8,8 +8,6 @@ const LeafletMap = () => {
   useEffect(() => {
     if (mapRef.current) {
       const map = mapRef.current.leafletElement;
-      // Customize the map if needed
-      // map.setView([latitude, longitude], zoomLevel);
     }
   }, []);
 
@@ -17,6 +15,11 @@ const LeafletMap = () => {
     <div className='leaflet-container'>
     <MapContainer ref={mapRef} center={[39.290440, -76.612328]} zoom={13} style={{ height: '400px', width: '100%' }}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Marker position={[39.290440, -76.612328]}>
+        <Popup position={[39.290440, -76.612328]}>
+          <div><h5>Canton</h5></div>
+        </Popup>
+      </Marker>
     </MapContainer>
     </div>
   );
