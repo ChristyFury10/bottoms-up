ff6195ba78e1a3722a1f0acdb8bfe46ace0b4dcb
ChristyFury10 <104020980+ChristyFury10@users.noreply.github.com>
2023-09-02 19:59:16 -0400

    Revert "adds aws backend fetch calls"

--- bottoms_up/frontend/src/pages/BarUpdatePage.js
+++ bottoms_up/frontend/src/pages/BarUpdatePage.js
@@ -23,7 +23,7 @@ const BarUpdatePage = ({bar, setBar}) => {
 
     
     let getBar = async () => {
-        let response = await fetch(`http://18.116.241.164:8000/api/bars/${id}`)
+        let response = await fetch(`/api/bars/${id}`)
         let data = await response.json()
         setBar(data)
         const{name, address, hours, specials} = data
@@ -50,14 +50,14 @@ const BarUpdatePage = ({bar, setBar}) => {
 		}
         console.log("barState:", bar)
 
-        await fetch(`http://18.116.241.164:8000/api/bars/${id}/update`, options);
+        await fetch(`/api/bars/${id}/update`, options);
         
         navigate(`/bars/${id}`)
     
         
     }
     const deleteBar = async ()=>{
-            fetch(`http://18.116.241.164:8000/api/bars/${id}/delete`, {
+            fetch(`/api/bars/${id}/delete`, {
                 method: 'DELETE', 
                 'headers': {
                     'Content-Type' :'application/json'
